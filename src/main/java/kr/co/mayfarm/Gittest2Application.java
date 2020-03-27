99beca8b4e69adf363005544bbacebbb3249b6b7
irnd04 <irnd04@gmail.com>
2020-03-27 14:08:08 +0900

    first commit

---
+++ src/main/java/kr/co/mayfarm/Gittest2Application.java
@@ -0,0 +1,19 @@
+package kr.co.mayfarm;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittest2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gittest2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello");
+	}
+
+}
